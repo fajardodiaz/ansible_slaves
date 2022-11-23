0dc50e817fa91750e4ccb8f9a308e718648bdb0c
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-11-23 14:33:40 -0600

    Server config with rmi keystore

--- edit-server.sh
+++ edit-server.sh
@@ -4,3 +4,4 @@ sudo sed -i "s/#server_port=1099/server_port=10991/" /opt/apache-jmeter-5.5/bin/
 sudo sed -i "s/# - set server_port=1234/- set server_port=10991/" /opt/apache-jmeter-5.5/bin/jmeter.properties
 sudo sed -i "s/# - start rmiregistry with port 1234/- start rmiregistry with port 10991/" /opt/apache-jmeter-5.5/bin/jmeter.properties
 sudo sed -i "s/# SERVER_PORT=1234 jmeter-server/SERVER_PORT=10991 jmeter-server/" /opt/apache-jmeter-5.5/bin/jmeter.properties
+sudo sed -i "s/#server.rmi.ssl.keystore.file=rmi_keystore.jks/server.rmi.ssl.keystore.file=rmi_keystore.jks/" /opt/apache-jmeter-5.5/bin/jmeter.properties
