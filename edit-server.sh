5c4ac00bc9f80e7b8dba9123f556271b3775b938
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-11-23 14:21:30 -0600

    Server config

--- edit-server.sh
+++ edit-server.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sudo sed -i "s/#server.rmi.ssl.disable=false/server.rmi.ssl.disable=true/" /opt/apache-jmeter-5.5/bin/jmeter.properties
+sudo sed -i "s/#server_port=1099/server_port=10991/" /opt/apache-jmeter-5.5/bin/jmeter.properties
+sudo sed -i "s/# - set server_port=1234/- set server_port=10991/" /opt/apache-jmeter-5.5/bin/jmeter.properties
+sudo sed -i "s/# - start rmiregistry with port 1234/- start rmiregistry with port 10991/" /opt/apache-jmeter-5.5/bin/jmeter.properties
+sudo sed -i "s/# SERVER_PORT=1234 jmeter-server/SERVER_PORT=10991 jmeter-server/" /opt/apache-jmeter-5.5/bin/jmeter.properties
