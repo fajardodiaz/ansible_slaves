5e7a2e69c732f54865f2223cb425f3461327cff7
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-12-06 08:36:49 -0500

    Fix to only run 1 command

--- edit-server-worker.sh
+++ edit-server-worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-MYSTRING=11.0.1.207,11.0.1.220
+MYSTRING=11.0.1.x,11.0.1.x
 sudo sed -i "s/#remote_hosts=127.0.0.1/remote_hosts=$MYSTRING/" /opt/apache-jmeter-5.5/bin/jmeter.properties
